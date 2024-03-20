f5ba8c819aaa254f06bf9e39cd91ee584cc2dd33
Goran Ninkovic <g.ninkovic@angeltech.rs>
2024-03-20 20:46:17 +0100

    tools: add eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@pinia/nuxt"],
+  modules: ["@nuxtjs/eslint-module", "@nuxt/ui", "@pinia/nuxt"],
   colorMode: {
     preference: "light",
   },

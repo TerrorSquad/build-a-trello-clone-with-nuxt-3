001b8a7e3d7bc2b2b96c83c7cea572bc0056de79
Goran Ninkovic <g.ninkovic@angeltech.rs>
2024-03-20 20:40:35 +0100

    chore: finish project setup

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <h1>Build a trello clone home page</h1>
+    <pre>{{ boardStore.board }}</pre>
+  </main>
+</template>
+
+<script setup lang="ts">
+const boardStore = useBoardStore();
+</script>
+
+<style scoped></style>
